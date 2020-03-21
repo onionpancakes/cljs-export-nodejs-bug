e597140caebbbb191f57b7e1b69b97253e4d9344
Gordon Lin <gordonlin89@gmail.com>
2020-03-21 15:20:05 -0700

    Node script; build aliases in deps

---
+++ run.js
@@ -0,0 +1,5 @@
+const mylib = require('./out/main')
+
+console.log(mylib)
+
+mylib.foo.bar.baz()
--- src/foo/bar.cljs
+++ src/foo/bar.cljs
@@ -1,4 +1,4 @@
 (ns foo.bar)
 
 (defn ^:export baz []
-  (js/console.log "baz"))
+  (js/console.log "It works!"))
